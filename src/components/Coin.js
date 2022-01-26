03d01ed1f361061b8fb557e0e6823253f5337034
RanaMuzamal <muzamilrana149@gmail.com>
2022-01-26 16:56:18 +0500

    updated code

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Coin({ name, icon, price, symbol }) {
     return <div className='coinDetail'>
-        <h1>{name}</h1>
+        <h2>{name}</h2>
         <img src={icon} />
         <h2>{symbol}</h2>
         <h2>Price:{price}</h2>
